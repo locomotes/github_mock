854e10e1ec9094fc993cce7a4908c760c8583517
Jonathan Dickerson <jonathandickerson@Jonathans-MacBook-Pro-4.local>
2014-06-18 18:16:57 -0400

    repo data feed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('The Iron Yard Rocks');
+
+
+
+var name ,lastupdated, language, stargazers, forks;
+repo.forEach(function(a) {
+	// fork = '<div class="fork">' + a.fork + '</div>';
+	name = '<div class="name">' + a.name + '</div>';
+	lastupdated = '<div class="name">' + a.updated_at + '</div>';
+	language = '<div class="language">' + a.language + '</div>';
+	stargazers = '<div class="stargazers">' + a.stargazers_count + '</div>';
+	forks = '<div class="forks">' + a.forks + '</div>';
+	$('.results').append('<div class="box">' + name + lastupdated + language + stargazers + '<span class="octicon octicon-git-branch"></span>' + forks +'</div>');
+});
