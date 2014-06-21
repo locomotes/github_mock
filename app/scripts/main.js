a73a563a668fcbaf271cbcb780d31d6e3a766335
Jonathan Dickerson <jonathandickerson@Jonathans-MacBook-Pro-4.local>
2014-06-21 15:30:56 -0400

    middle repo feed working with underscore templating

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,48 @@
+var cID = clientID || '';
+var cSECRET = clientSecret || '';
 
+var jon = _.template($('#sidebar_template').html() );
 
+$.getJSON('https://api.github.com/users/locomotes', {client_ID: cID, client_secret: cSECRET}, function (data){
+	
+	//$('#sidebar_template').append(renderTemplate(template));
 
-var name ,lastupdated, language, stargazers, forks;
-repo.forEach(function(a) {
-	// fork = '<div class="fork">' + a.fork + '</div>';
-	name = '<div class="name">' + a.name + '</div>';
-	lastupdated = '<div class="name">' + a.updated_at + '</div>';
-	language = '<div class="language">' + a.language + '</div>';
-	stargazers = '<div class="stargazers">' + a.stargazers_count + '</div>';
-	forks = '<div class="forks">' + a.forks + '</div>';
-	$('.results').append('<div class="box">' + name + lastupdated + language + stargazers + '<span class="octicon octicon-git-branch"></span>' + forks +'</div>');
+	$("#columnone").append( jon(data) );
+
 });
+
+var repos = _.template($('#repos').html() );
+
+$.getJSON('https://api.github.com/users/locomotes/repos', {client_ID: cID, client_secret: cSECRET}, function (data){
+	
+	_.each(data, function(a){
+	$(".results").append( repos(a) );
+	});
+
+});
+
+
+
+// $.getJSON('https://api.github.com/users/locomotes/repos', {client_ID: cID, client_secret: cSECRET}, function (data){
+
+// 	var name ,lastupdated, language, stargazers, forks;
+// 	data.forEach(function(a) {
+// 		fork = '<div class="fork">' + '<span class="octicon octicon-repo"></span>' + a.fork + '</div>';
+// 		name = '<div class="name">' + a.name + '</div>';
+// 		lastupdated = '<div class="lastupdated">' + a.updated_at + '</div>';
+// 		language = '<div class="language">' + a.language + '</div>';
+// 		stargazers = '<div class="stargazers">' + a.stargazers_count + '</div>';
+// 		forks = '<div class="forks">' + a.forks + '</div>';
+// 		$('.results').append('<div class="box">' + name + lastupdated +'</div>');
+// 		$('.results').append('<div class="box2">'+ language + stargazers + forks +'</div>');
+// 	});
+
+// });
+
+
+	
+
+// $.ajax({
+//   url: "https://avatars.githubusercontent.com/u/5142640?",
+//   dataType : 'json'
+// });
