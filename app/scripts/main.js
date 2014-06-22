c6c4b8f079b24045546d3d737eb26a44ea800d50
Jonathan Dickerson <jonathandickerson@Jonathans-MacBook-Pro-4.local>
2014-06-22 16:29:15 -0400

    mid container above above repo feed complete except for search bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,29 +20,3 @@ $.getJSON('https://api.github.com/users/locomotes/repos', {client_ID: cID, clien
 	});
 
 });
-
-
-
-// $.getJSON('https://api.github.com/users/locomotes/repos', {client_ID: cID, client_secret: cSECRET}, function (data){
-
-// 	var name ,lastupdated, language, stargazers, forks;
-// 	data.forEach(function(a) {
-// 		fork = '<div class="fork">' + '<span class="octicon octicon-repo"></span>' + a.fork + '</div>';
-// 		name = '<div class="name">' + a.name + '</div>';
-// 		lastupdated = '<div class="lastupdated">' + a.updated_at + '</div>';
-// 		language = '<div class="language">' + a.language + '</div>';
-// 		stargazers = '<div class="stargazers">' + a.stargazers_count + '</div>';
-// 		forks = '<div class="forks">' + a.forks + '</div>';
-// 		$('.results').append('<div class="box">' + name + lastupdated +'</div>');
-// 		$('.results').append('<div class="box2">'+ language + stargazers + forks +'</div>');
-// 	});
-
-// });
-
-
-	
-
-// $.ajax({
-//   url: "https://avatars.githubusercontent.com/u/5142640?",
-//   dataType : 'json'
-// });
